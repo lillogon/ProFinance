45eaab87b5802815b86970b7a85865bdfb7211c0
Murillo Gonçalves <lillogon@outlook.com>
2026-03-25 19:53:36 -0300

    feat: first commit

---
+++ src/main/java/com/lillogon/pro_finance/ProFinanceApplication.java
@@ -0,0 +1,13 @@
+package com.lillogon.pro_finance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProFinanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProFinanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lillogon/pro_finance/ProFinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lillogon.pro_finance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProFinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
