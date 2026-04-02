46910e8629c47805c89790844e91c8df7a18172a
Murillo Gonçalves <lillogon@outlook.com>
2026-04-01 21:20:36 -0300

    feat(category): implement input validation and global exception handling

---
+++ src/main/java/com/lillogon/pro_finance/infra/RestExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.lillogon.pro_finance.infra;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class RestExceptionHandler {
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<RestErrorMessage> handleJsonError(HttpMessageNotReadableException exception){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new RestErrorMessage(HttpStatus.BAD_REQUEST, "Invalid JSON format."));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<RestErrorMessage> handleGenericError(Exception exception) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new RestErrorMessage(HttpStatus.INTERNAL_SERVER_ERROR, "Unexpected error occurred."));
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<RestErrorMessage> handleBadRequest(IllegalArgumentException exception){
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new RestErrorMessage(HttpStatus.BAD_REQUEST, exception.getMessage()));
+    }
+}
--- src/main/java/com/lillogon/pro_finance/service/CategoryService.java
+++ src/main/java/com/lillogon/pro_finance/service/CategoryService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.time.format.DecimalStyle;
 
 @Service
 public class CategoryService {
@@ -16,10 +17,21 @@ public class CategoryService {
 
     public Category createCategory(CategoryRequestDTO data){
         Category newCategory = new Category();
-        newCategory.setDescription(data.description());
-        newCategory.setActive(data.active());
-        newCategory.setCreatedAt(LocalDateTime.now());
-        newCategory.setUpdatedAt(LocalDateTime.now());
+        LocalDateTime now = LocalDateTime.now();
+
+        if (data.description() == null || data.description().isBlank()){
+            throw new IllegalArgumentException("Description cannot be null or empty.");
+        }
+
+        boolean active = data.active() != null ? data.active() : true;
+
+        newCategory.setDescription(data.description().toLowerCase().trim());
+        newCategory.setActive(active);
+        newCategory.setCreatedAt(now);
+        newCategory.setUpdatedAt(now);
+        if (!active){
+            newCategory.setBlockedAt(now);
+        }
 
         repository.save(newCategory);
 
