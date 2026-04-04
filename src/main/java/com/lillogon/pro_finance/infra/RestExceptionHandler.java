b7e43dcd586cde05120e701764f7b41f7a6ed8a8
Murillo Gonçalves <lillogon@outlook.com>
2026-04-03 22:51:12 -0300

    feat: add ResourceNotFoundException and 404 handler

--- src/main/java/com/lillogon/pro_finance/infra/RestExceptionHandler.java
+++ src/main/java/com/lillogon/pro_finance/infra/RestExceptionHandler.java
@@ -1,11 +1,11 @@
 package com.lillogon.pro_finance.infra;
 
+import com.lillogon.pro_finance.exceptions.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
 public class RestExceptionHandler {
@@ -16,17 +16,24 @@ public class RestExceptionHandler {
                 .body(new RestErrorMessage(HttpStatus.BAD_REQUEST, "Invalid request."));
     }
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<RestErrorMessage> handleGenericError(Exception exception) {
-        return ResponseEntity
-                .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(new RestErrorMessage(HttpStatus.INTERNAL_SERVER_ERROR, "Unexpected error occurred."));
-    }
-
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<RestErrorMessage> handleBadRequest(IllegalArgumentException exception){
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
                 .body(new RestErrorMessage(HttpStatus.BAD_REQUEST, exception.getMessage()));
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<RestErrorMessage> handleResourceNotFound(ResourceNotFoundException exception){
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(new RestErrorMessage(HttpStatus.NOT_FOUND, exception.getMessage()));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<RestErrorMessage> handleGenericError(Exception exception) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new RestErrorMessage(HttpStatus.INTERNAL_SERVER_ERROR, "Unexpected error occurred."));
     }
 }
