1b27258dcb497704e66090dbaf7d9e9eb99e9d75
Murillo Gonçalves <lillogon@outlook.com>
2026-06-05 22:00:47 -0300

    feat: add repositories

---
+++ src/main/java/com/lillogon/profinance/repository/TransactionLogRepository.java
@@ -0,0 +1,11 @@
+package com.lillogon.profinance.repository;
+
+import com.lillogon.profinance.entity.TransactionLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface TransactionLogRepository extends JpaRepository<TransactionLog, UUID> {
+}
