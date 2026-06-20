f9a9af9932594eecbf42a4543ffe0d25a3697a0f
Murillo Gonçalves <lillogon@outlook.com>
2026-06-20 15:16:15 -0300

    feat: add PartyRepository findByDocumentNumber and PartyUpdateDTO

--- src/main/java/com/lillogon/profinance/repository/PartyRepository.java
+++ src/main/java/com/lillogon/profinance/repository/PartyRepository.java
@@ -4,8 +4,10 @@ import com.lillogon.profinance.entity.Party;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface PartyRepository extends JpaRepository<Party, UUID> {
+    Optional<Party> findByDocumentNumber(String documentNumber);
 }
